db71a7395fb7d652b6af51d3e39a115ff736e242
dwdyer <dan@uncommons.org>
2014-01-04 00:08:53 +0000

    Deploy to multiple devices/emulators in parallel.

--- src/java/main/com/rectangularsoftware/antlib/tasks/InstallAPK.java
+++ src/java/main/com/rectangularsoftware/antlib/tasks/InstallAPK.java
@@ -8,6 +8,12 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
@@ -33,17 +39,30 @@ public class InstallAPK extends Task
         try
         {
             List<String> devices = getDeviceIdentifiers();
-            for (String device : devices)
+            System.out.printf("Installing %s on %d devices...%n", apkFile, devices.size());
+            ExecutorService executor = Executors.newFixedThreadPool(devices.size());
+            List<Future<Void>> futures = new ArrayList<Future<Void>>(devices.size());
+            for (final String device : devices)
             {
-                System.out.printf("Installing APK on %s...%n", device);
-                Process process = Runtime.getRuntime().exec(new String[]{"adb", "-s", device, "install", "-r", apkFile.toString()});
-                consumeStream(process.getInputStream(), System.out);
-                if (process.waitFor() != 0)
+                futures.add(executor.submit(new Callable<Void>()
                 {
-                    consumeStream(process.getErrorStream(), System.err);
-                    throw new BuildException(String.format("Installing APK on %s failed.", device));
-                }
+                    public Void call() throws IOException, InterruptedException
+                    {
+                        installOnDevice(device);
+                        return null;
+                    }
+                }));
             }
+            for (Future<Void> future : futures)
+            {
+                future.get();
+            }
+            executor.shutdown();
+            executor.awaitTermination(60, TimeUnit.SECONDS);
+        }
+        catch (ExecutionException ex)
+        {
+            throw new BuildException(ex.getCause());
         }
         catch (IOException ex)
         {
@@ -56,14 +75,33 @@ public class InstallAPK extends Task
     }
 
 
-    private void consumeStream(InputStream in, PrintStream out) throws IOException
+    private void installOnDevice(String device) throws IOException, InterruptedException
+    {
+        Process process = Runtime.getRuntime().exec(new String[]{"adb", "-s", device, "install", "-r", apkFile.toString()});
+        consumeStream(process.getInputStream(), System.out, device);
+        if (process.waitFor() != 0)
+        {
+            consumeStream(process.getErrorStream(), System.err, device);
+            throw new BuildException(String.format("Installing APK on %s failed.", device));
+        }
+    }
+
+
+    private void consumeStream(InputStream in, PrintStream out, String tag) throws IOException
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
         try
         {
             for (String line = reader.readLine(); line != null; line = reader.readLine())
             {
-                out.println(line);
+                if (tag != null)
+                {
+                    out.printf("[%s] %s%n", tag, line.trim());
+                }
+                else
+                {
+                    out.println(line);
+                }
             }
         }
         finally
@@ -89,7 +127,7 @@ public class InstallAPK extends Task
             }
             if (process.waitFor() != 0)
             {
-                consumeStream(process.getErrorStream(), System.err);
+                consumeStream(process.getErrorStream(), System.err, null);
                 throw new BuildException("Failed getting list of connected devices/emulators.");
             }
         }
